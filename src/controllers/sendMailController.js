222659136b7d4022d807935cbbdcb103d3defb99
Priyanshu <chaurasiyapriyanshu66@gmail.com>
2025-10-05 18:47:23 +0530

    mail controller fixed for https with port

--- src/controllers/sendMailController.js
+++ src/controllers/sendMailController.js
@@ -9,7 +9,10 @@ const sendMail = async (req, res) => {
 
     try {
         let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
             service: "gmail",
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.EMAIL,
                 pass: process.env.EMAIL_PASSWORD
