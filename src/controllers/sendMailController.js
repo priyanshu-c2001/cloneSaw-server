cd5ad48089c751aa6a381b5b822c8daeb95ce513
Priyanshu <chaurasiyapriyanshu66@gmail.com>
2025-10-05 22:10:03 +0530

    change transporter code

--- src/controllers/sendMailController.js
+++ src/controllers/sendMailController.js
@@ -8,16 +8,12 @@ const sendMail = async (req, res) => {
     }
 
     try {
-        let transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-            secure: false,
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
             auth: {
                 user: process.env.EMAIL,
-                pass: process.env.EMAIL_PASSWORD,
-            },
-            logger: true,   
-            debug: true     
+                pass: process.env.EMAIL_PASSWORD
+            }
         });
 
         await transporter.sendMail({
