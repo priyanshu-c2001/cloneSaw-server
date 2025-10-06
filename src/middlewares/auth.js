7aa32dc98afd6e0dd8625234a6f2a06de8b3c73e
Priyanshu <chaurasiyapriyanshu66@gmail.com>
2025-10-06 14:36:37 +0530

    cookie issue fixing

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,26 +3,29 @@ const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
     try {
-        const { token } = req.cookies;
+        const cookie = req.cookies;
+        const { token } = cookie;
 
         if (!token) {
-            return res.status(401).json({ message: "Please Login!" });
+            res.status(401).json({ message: "Please Login!" });
         }
 
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         const { _id } = decodedToken;
-
-        const user = await User.findById(_id);
+        const user = await User.findById({ _id: _id });
 
         if (!user) {
-            return res.status(404).json({ message: "No User Exist!!" });
+            throw new Error("No User Exist!!");
         }
 
         req.user = user;
+
         next();
     } catch (err) {
-        return res.status(400).json({ message: "ERROR: " + err.message });
+        res.status(400).send("ERROR" + err.message);
     }
-};
+}
 
-module.exports = auth;
+module.exports = {
+    auth,
+}
